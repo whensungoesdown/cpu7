cad50bb925b817db16980a1f6b1974814cdaf3ec
uty <whensungoes@gmail.com>
2023-01-07 01:05:03 -0500

    add test case pcaddu12i

--- sims/verilator/run_func/configure.sh
+++ sims/verilator/run_func/configure.sh
@@ -357,6 +357,11 @@ do
             mkdir -p ./obj/func
             mkdir -p ./log/func
             ;;
+        func/func_uty20_pcaddu12i) 
+            RUN_FUNC=y
+            mkdir -p ./obj/func
+            mkdir -p ./log/func
+            ;;
         my_program)
             RUN_FUNC=n 
             RUN_C=y
