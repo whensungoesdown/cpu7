8e6c921f1df2dd8f405e71be34e942a88a463335
uty <whensungoes@gmail.com>
2023-01-10 11:00:47 -0500

    add test case func/func_uty39_beq

--- sims/verilator/run_func/run_all_tests.sh
+++ sims/verilator/run_func/run_all_tests.sh
@@ -271,3 +271,10 @@ movetotrash ./obj/func/
 ./configure.sh -run func/func_uty38_sraiw --disable-trace-comp
 make soft_compile
 make simulation_run_func
+
+
+echo -e "\n\n\nTest func_uty39_beq\n"
+movetotrash ./obj/func/
+./configure.sh -run func/func_uty39_beq --disable-trace-comp
+make soft_compile
+make simulation_run_func
